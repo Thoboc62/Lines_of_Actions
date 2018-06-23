689b877142822bb13f3536a4a5f70a14931f3521
Thoboc62 <tbocquelet@gmail.com>
2018-06-23 10:28:11 +0200

    Affichage statistiques IA Rand VS IA Rand

--- test/test_lines_of_actions_random.mjs
+++ test/test_lines_of_actions_random.mjs
@@ -1,5 +1,9 @@
 import lib from '../lib/openxum-core/openxum';
 
+let nb_victoires_white = 0;
+let nb_victoires_black = 0;
+let nb_coups = 0;
+
 for(let i=0; i<100; ++i) {
   let e = new lib.OpenXum.LinesOfActions.Engine(lib.OpenXum.LinesOfActions.GameType.STANDARD, lib.OpenXum.LinesOfActions.Color.BLACK);
   let p1 = new lib.OpenXum.RandomPlayer(lib.OpenXum.LinesOfActions.Color.BLACK, e);
@@ -15,9 +19,23 @@ for(let i=0; i<100; ++i) {
     p = p === p1 ? p2 : p1;
   }
 
+  if(e.winner_is() === 1) {
+    ++nb_victoires_white;
+  }
+  else{
+    ++nb_victoires_black;
+  }
+
   console.log("Winner is " + (e.winner_is() === lib.OpenXum.LinesOfActions.Color.BLACK ? "player 1" : "player 2"));
   console.log(moves.length);
+  nb_coups = nb_coups + moves.length;
   /*for (let index = 0; index < moves.length; ++index) {
     console.log(moves[index].to_string());
   }*/
 }
+
+let nb_coups_moyen = nb_coups / 100;
+
+console.log("NB victoires BLANC: " + nb_victoires_white);
+console.log("NB victoires NOIR: " + nb_victoires_black);
+console.log("NB coups moyen: " + nb_coups_moyen);
